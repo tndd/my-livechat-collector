a7e09f03325906eb1bfb76b86ad4c8c511d1a84f
tan <ttru702@gmail.com>
2021-02-18 17:02:18 +0900

    test youtube-data-api-search-list

---
+++ channel_video_list/sample_youtube_data_api_search_list.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+
+import googleapiclient.discovery
+import googleapiclient.errors
+
+load_dotenv('.env')
+
+def main():
+  api_service_name = "youtube"
+  api_version = "v3"
+  api_key = os.environ.get("YOUTUBE_DATA_API_KEY")
+
+  youtube = googleapiclient.discovery.build(
+    api_service_name,
+    api_version,
+    developerKey=api_key
+  )
+
+  request = youtube.search().list(
+    part="snippet",
+    channelId="UCvaTdHTWBGv3MKj3KVqJVCw",
+    eventType="completed",
+    maxResults=50,
+    order="date",
+    type="video"
+  )
+  response = request.execute()
+
+  print(response)
+
+if __name__ == "__main__":
+  main()
