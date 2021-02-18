5b98fc11e34c948251c55ab67c3674a31500c8d1
tan <ttru702@gmail.com>
2021-02-18 20:43:22 +0900

    test convert response_json to yaml for gcs bucket

---
+++ channel_video_list/sample_upload_gcs.py
@@ -0,0 +1,22 @@
+import json
+import yaml
+
+fname = 'output_youtube_data_api_search_list.json'
+with open(fname, 'r') as f:
+  data = json.loads(f.read())
+
+channel_data = {}
+channel_data['channel_id'] = data['items'][0]['snippet']['channelId']
+channel_data['channel_title'] = data['items'][0]['snippet']['channelTitle']
+
+videos_data = {}
+for d in data.get('items', []):
+  videos_data[d['id']['videoId']] = {
+    'title': d['snippet']['title'],
+    'published_at': d['snippet']['publishedAt']
+  }
+
+channel_data.update(videos_data=videos_data)
+
+with open('sample_channel_video_list_out.yml', 'w') as f:
+  yaml.dump(channel_data, f, allow_unicode=True)
