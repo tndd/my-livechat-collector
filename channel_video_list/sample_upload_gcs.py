870dd006adbda6e537f97d08c172d87ccc66fa31
tan <ttru702@gmail.com>
2021-02-19 12:52:15 +0900

    change indent size 4

--- channel_video_list/sample_upload_gcs.py
+++ channel_video_list/sample_upload_gcs.py
@@ -3,7 +3,7 @@ import yaml
 
 fname = 'output_youtube_data_api_search_list.json'
 with open(fname, 'r') as f:
-  data = json.loads(f.read())
+    data = json.loads(f.read())
 
 channel_data = {}
 channel_data['channel_id'] = data['items'][0]['snippet']['channelId']
@@ -11,12 +11,12 @@ channel_data['channel_title'] = data['items'][0]['snippet']['channelTitle']
 
 videos_data = {}
 for d in data.get('items', []):
-  videos_data[d['id']['videoId']] = {
-    'title': d['snippet']['title'],
-    'published_at': d['snippet']['publishedAt']
-  }
+    videos_data[d['id']['videoId']] = {
+        'title': d['snippet']['title'],
+        'published_at': d['snippet']['publishedAt']
+    }
 
 channel_data.update(videos_data=videos_data)
 
 with open('sample_channel_video_list_out.yml', 'w') as f:
-  yaml.dump(channel_data, f, allow_unicode=True)
+    yaml.dump(channel_data, f, allow_unicode=True)
--- channel_video_list/sample_youtube_data_api_search_list.py
+++ channel_video_list/sample_youtube_data_api_search_list.py
@@ -8,27 +8,28 @@ import googleapiclient.errors
 load_dotenv('.env')
 
 def main():
-  api_service_name = "youtube"
-  api_version = "v3"
-  api_key = os.environ.get("YOUTUBE_DATA_API_KEY")
+    api_service_name = "youtube"
+    api_version = "v3"
+    api_key = os.environ.get("YOUTUBE_DATA_API_KEY")
 
-  youtube = googleapiclient.discovery.build(
-    api_service_name,
-    api_version,
-    developerKey=api_key
-  )
+    youtube = googleapiclient.discovery.build(
+        api_service_name,
+        api_version,
+        developerKey=api_key
+    )
 
-  request = youtube.search().list(
-    part="snippet",
-    channelId="UCvaTdHTWBGv3MKj3KVqJVCw",
-    eventType="completed",
-    maxResults=50,
-    order="date",
-    type="video"
-  )
-  response = request.execute()
+    request = youtube.search().list(
+        part="snippet",
+        channelId="UCvaTdHTWBGv3MKj3KVqJVCw",
+        eventType="completed",
+        maxResults=50,
+        order="date",
+        type="video"
+    )
+    response = request.execute()
 
-  print(response)
+    print(response)
+
 
 if __name__ == "__main__":
-  main()
+    main()
