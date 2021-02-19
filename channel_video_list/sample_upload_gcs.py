a80411a3750e899951df82ff7d3f9225967b2dab
tan <ttru702@gmail.com>
2021-02-19 16:49:09 +0900

    test upload to gcs

--- channel_video_list/sample_upload_gcs.py
+++ channel_video_list/sample_upload_gcs.py
@@ -1,5 +1,11 @@
 import json
-import yaml
+import os
+
+from dotenv import load_dotenv
+
+from google.cloud import storage
+
+load_dotenv('.env')
 
 fname = 'output_youtube_data_api_search_list.json'
 with open(fname, 'r') as f:
@@ -18,5 +24,13 @@ for d in data.get('items', []):
 
 channel_data.update(videos_data=videos_data)
 
-with open('sample_channel_video_list_out.yml', 'w') as f:
-    yaml.dump(channel_data, f, allow_unicode=True, indent=4)
+client = storage.Client.from_service_account_json(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+bucket_name = os.environ.get('BUCKET_NAME')
+
+bucket = client.get_bucket(bucket_name)
+blob = bucket.blob(f"channel-video-list/{channel_data['channel_id']}.json")
+
+blob.upload_from_string(
+    data=json.dumps(channel_data, indent=4, ensure_ascii=False),
+    content_type='application/json'
+)
